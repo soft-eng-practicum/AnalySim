fb42978dbf09f502dd2f5a10f81209434d0e9325
Hieu Dinh <hiiamaalien@gmail.com>
2020-03-11 19:30:42 -0400

    Add Cors

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,16 @@ namespace NeuroSimHub
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            //Enable Cors
+            services.AddCors(options =>
+            {
+                options.AddPolicy("EnableCORS", builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +51,8 @@ namespace NeuroSimHub
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors("EnableCORS");
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
