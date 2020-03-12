92bc24dd38efbda6bbc211b8a2f54f0772c01a52
Hieu Dinh <hiiamaalien@gmail.com>
2020-03-12 01:08:42 -0400

    add User Register Method

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NeuroSimHub.Models;
 
 namespace NeuroSimHub.Controllers
 {
@@ -19,8 +20,43 @@ namespace NeuroSimHub.Controllers
         {
             this._userManager = _userManager;
             this._signManager = _signManager;
+        }
 
+        
+        [HttpPost("action")]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel formdata) {
+            
+            //Hold Error
+            List<string> errorList = new List<string>();
+
+            var user = new IdentityUser
+            {
+                Email = formdata.EmailAddress,
+                UserName = formdata.Username,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var result = await _userManager.CreateAsync(user, formdata.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Customer");
+
+                return Ok(new { username = user.UserName, email = user.Email, status = 1, message = "Registration Successful" });
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                    errorList.Add(error.Description);
+                }
+            }
+
+            return BadRequest(new JsonResult(errorList));
 
         }
+
+
     }
 }
