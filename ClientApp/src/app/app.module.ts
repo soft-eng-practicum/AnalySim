81278b2d198f67e5453796b11b4a30d9561fb2f1
Hieu Dinh <hiiamaalien@gmail.com>
2020-03-18 19:51:11 -0400

    Worked on Login Component ts

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+
+
 
 @NgModule({
   declarations: [
@@ -20,6 +25,9 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ClientApp/src/app/login/login.component.ts
+++ ClientApp/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup} from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 import { AccountService } from '../services/account.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,12 +10,34 @@ import { AccountService } from '../services/account.service';
 })
 export class LoginComponent implements OnInit {
 
-  private loginStatusGet : boolean;
+  insertForm: FormGroup;
+  Username: FormControl;
+  Password: FormControl;
+  returnUrl: string;
+  ErrorMessage: string;
+  invalidLogin: boolean;
 
-  constructor(private acct : AccountService) { }
+  constructor(private acct : AccountService, 
+              private router : Router,
+              private route : ActivatedRoute,
+              private formBuilder : FormBuilder
+              ) { }
 
   ngOnInit() {
+    // Initialize Form Controls
+    this.Username = new FormControl('', [Validators.required]);
+    this.Password = new FormControl('', [Validators.required]);
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+
+     // Initialize FormGroup using FormBuilder
+    this.insertForm = this.formBuilder.group({
+        "Username" : this.Username,
+        "Password" : this.Password
     
+    });
+
   }
 
 }
