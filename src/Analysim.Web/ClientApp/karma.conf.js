113add25ea415efabc374a49c9e1f7adc4c380df
Hieu Dinh <hiiamaalien@gmail.com>
2022-06-27 03:01:13 -0400

    Update name

--- src/Analysim.Web/ClientApp/karma.conf.js
+++ src/Analysim.Web/ClientApp/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/client-app2'),
+      dir: require('path').join(__dirname, './coverage/client-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
