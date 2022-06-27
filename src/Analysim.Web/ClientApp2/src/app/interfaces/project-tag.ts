5b3cdbe3f73c2bc776dcba685552028fa3bceb3e
Hieu Dinh <hiiamaalien@gmail.com>
2022-06-27 02:58:00 -0400

    Replace Client App Folder

--- src/Analysim.Web/ClientApp2/src/app/interfaces/project-tag.ts
+++
@@ -1,9 +0,0 @@
-import { Tag } from 'src/app/interfaces/tag';
-import { Project } from 'src/app/interfaces/project';
-
-export interface ProjectTag {
-    tagID: number;
-    tag: Tag;
-    projectID: number;
-    project: Project;
-}
--- src/Analysim.Web/ClientApp2/src/app/interfaces/tag.ts
+++
@@ -1,7 +0,0 @@
-import { ProjectTag } from 'src/app/interfaces/project-tag';
-
-export interface Tag {
-    tagID: number;
-    name: string;
-    projectTags: Array<ProjectTag>;
-}
