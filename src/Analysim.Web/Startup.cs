f6efdfcd5a0706fccd03e50c01fd225bac45346f
kiyros <jongchangco@ggc.edu>
2022-06-29 02:09:51 -0400

    attempt to update to .NET6
    - password issue detected
    - database issue?

--- src/Analysim.Web/Startup.cs
+++ src/Analysim.Web/Startup.cs
@@ -105,8 +105,7 @@ namespace Web
 
                 if (env.IsDevelopment())
                 {
-                    //spa.UseAngularCliServer(npmScript: "start");
-                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+                    spa.UseAngularCliServer(npmScript: "start");
                 }
 
             });
