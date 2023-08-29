05db4ac2f158d4f19c7c9b1f991694e4e2926e3b
ponrajaprabhusk <ponrajaprabhusk@gmail.com>
2023-08-29 12:37:28 +0530

    project-page-design

---
+++ src/Analysim.Web/ClientApp/src/app/projects/project-comments/project-comments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-comments',
+  templateUrl: './project-comments.component.html',
+  styleUrls: ['./project-comments.component.scss']
+})
+export class ProjectCommentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
