f9c29ac98f89bb952291ac6170be05bc6da26d7d
Tripp Barker <105750441+TrippBarker@users.noreply.github.com>
2024-06-14 16:27:55 -0400

    extracted searching for projects to explore service

--- src/Analysim.Web/ClientApp/src/app/services/explore.service.ts
+++ src/Analysim.Web/ClientApp/src/app/services/explore.service.ts
@@ -1,19 +1,45 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Project } from '../interfaces/project';
+import { ProjectService } from './project.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExploreService {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private projectService: ProjectService
+  ) { }
+
+  public projects : Project[]
+
+  searchProject(searchTerms : string[]){
+    this.projects = null
+    if(searchTerms.length == 0){
+      this.projectService.getProjectList().subscribe(
+        result =>{
+          this.projects = result
+        }, error =>{
+          console.log(error);      
+        });
+    }
+    else{
+      this.projectService.search(searchTerms).subscribe(
+        result =>{
+          this.projects = result
+        }, error =>{
+          console.log(error);      
+        });
+    }
+  }
 
   exploreProject(tagValue: string){
     const searchTerms: string[] = Array.from(
       new Set(tagValue.split(' ').filter(x => x.length !== 0))
     );
-    this.router.navigate(['/explore'], {
-      queryParams: { category: 'project', term: JSON.stringify(searchTerms)}
-    });
+    this.searchProject(searchTerms)
+    this.router.navigate(['/explore'], { queryParams: { category : 'project', term : JSON.stringify(searchTerms)}})
   }
 }
