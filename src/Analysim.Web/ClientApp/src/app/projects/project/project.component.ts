7f1ff484a9168d6a2875fedf7079d4e50e05b112
uday biswas <ub8280634471@gmail.com>
2024-09-04 15:50:16 +0530

    versioning update error resolved

--- src/Analysim.Web/ClientApp/src/app/projects/project/project.component.ts
+++ src/Analysim.Web/ClientApp/src/app/projects/project/project.component.ts
@@ -95,18 +95,20 @@ export class ProjectComponent implements OnInit {
           if (this.currentUser != null && this.project.projectUsers.find(x => x.userID == this.currentUser.id) != undefined) {
             this.projectUser = this.project.projectUsers.find(x => x.userID == this.currentUser.id)
           }
+          // console.log("Result : ", result);
           this.readmeNotebook = result.notebooks.filter((notebook) => notebook.name.toLowerCase() === "readme")[0];
+          // console.log("readme : " ,this.readmeNotebook);
           this.projectService.getNotebookVersions(result.notebooks.filter((notebook) => notebook.name.toLowerCase() === "readme")[0]).subscribe(versions => {
             this.versions = versions;
-            //console.log("Versions: ", this.versions);
+            // console.log("Versions: ", this.versions);
             if (this.versions.length > 0) {
               this.latestVersion = this.versions[0]; // Default to the latest version
             }
             // console.log("the loatest version  : ", this.latestVersion);
             this.projectService.getNotebookFile(result.notebooks.filter((notebook) => notebook.name.toLowerCase() === "readme")[0], this.latestVersion).subscribe(
               notebookJson => {
+                // console.log("the notebook content is : ", notebookJson);
                 this.notebookContent = this.sanitizer.bypassSecurityTrustHtml(this.renderNotebook(notebookJson));
-                // console.log("the notebook content is : ", this.notebookContent);
               });
           });
           // console.log("readme file is : ", result.notebooks.filter((notebook) => notebook.name.toLowerCase() === "readme")[0]);
@@ -177,25 +179,31 @@ export class ProjectComponent implements OnInit {
   }
 
   renderNotebook(notebookJson: any): string {
-    // Simple rendering of the notebook. Customize as needed.
+    // console.log("notebook rendering content :" , notebookJson);
     let htmlContent = `<div class="notebookClass">`;
     for (const cell of notebookJson.cells) {
       htmlContent += '<div class="notebook-cell">';
       if (cell.cell_type === 'markdown') {
-        htmlContent += marked(cell.source);
+        const markdownContent = Array.isArray(cell.source) ? cell.source.join('') : cell.source;
+        htmlContent += marked(markdownContent);
       } else if (cell.cell_type === 'code') {
-        htmlContent += '<pre><code><div>' + hljs.highlight(cell.source, {language: 'python'}).value + '</div></code></pre>';
+        // console.log("error: ", hljs.highlight(cell.source, {language: 'python'}));
+        const codeContent = Array.isArray(cell.source) ? cell.source.join('') : cell.source;
+        htmlContent += '<pre><code><div>' + hljs.highlight(codeContent, { language: 'python' }).value + '</div></code></pre>';
         if (cell.outputs) {
           for (const output of cell.outputs) {
             if (output.data && output.data['text/html']) {
-              htmlContent += output.data['text/html'];
+              const htmlOutput = Array.isArray(output.data['text/html']) ? output.data['text/html'].join('') : output.data['text/html'];
+              htmlContent += htmlOutput;
             } else if (output.data && output.data['image/png']) {
               htmlContent += `<img src="data:image/png;base64,${output.data['image/png']}" />`;
             } else if (output.data && output.data['text/plain']) {
-              htmlContent += '<pre>' + output.data['text/plain'] + '</pre>';
+              const plainTextOutput = Array.isArray(output.data['text/plain']) ? output.data['text/plain'].join('') : output.data['text/plain'];
+              htmlContent += '<pre>' + plainTextOutput + '</pre>';
             }
             else if (output.text) {
-              htmlContent += '<pre>' + output.text + '</pre>';
+              const outputText = Array.isArray(output.text) ? output.text.join('') : output.text;
+              htmlContent += '<pre>' + outputText + '</pre>';
             }
           }
         }
