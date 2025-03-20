c13687f5df36c81c15fbb4ae98e4cf86e8bf8ce8
Husain <husain.saify.met21@itbhu.ac.in>
2025-03-20 22:26:50 +0530

    Upload Issue resolved

--- src/Analysim.Web/Controllers/ProjectController.cs
+++ src/Analysim.Web/Controllers/ProjectController.cs
@@ -830,7 +830,7 @@ namespace Web.Controllers
                     Uri = "",
                     DateCreated = DateTimeOffset.UtcNow,
                     LastModified = DateTimeOffset.UtcNow,
-                    UserID = formdata.UserID,
+                    UserID = user.Id,
                     ProjectID = formdata.ProjectID
                 };
 
