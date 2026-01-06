b788e6f11ded170ea464725d6b1e15c14de7a18f
Cengiz Gunay <cengique@users.sf.net>
2026-01-06 17:22:21 +0000

    updated deploy scripts for use with multiple hosts

--- deploy/convert-pem2pfx.sh
+++ deploy/convert-pem2pfx.sh
@@ -1,1 +1,7 @@
-openssl pkcs12 -inkey /etc/letsencrypt/live/analysim.tech/privkey.pem -in /etc/letsencrypt/live/analysim.tech/fullchain.pem -export -out /etc/letsencrypt/live/analysim.tech/fullchain.pfx -password pass:analysim
+#! /bin/bash
+if [ "$1" == "" ]; then
+	echo "Usage: ./cert-renew <cert-dir>"
+	echo "Example: ./cert-renew /etc/letsencrypt/live/analysim.tech/"
+	exit -1
+fi
+openssl pkcs12 -inkey $1/privkey.pem -in $1/fullchain.pem -export -out $1/fullchain.pfx -password pass:analysim
