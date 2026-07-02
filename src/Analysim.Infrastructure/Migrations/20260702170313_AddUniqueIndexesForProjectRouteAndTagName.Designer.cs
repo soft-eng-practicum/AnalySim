381651aa42b53407527a44b05b7743acaf6b7723
Mohab <mohab_sobhy@outlook.com>
2026-07-02 20:11:08 +0300

    Add unique indexes for project routes and tag names

--- src/Analysim.Infrastructure/Migrations/20260702170313_AddUniqueIndexesForProjectRouteAndTagName.Designer.cs
+++ src/Analysim.Infrastructure/Migrations/20260702170313_AddUniqueIndexesForProjectRouteAndTagName.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Infrastructure.Migrations
+namespace Analysim.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260702170313_AddUniqueIndexesForProjectRouteAndTagName")]
+    partial class AddUniqueIndexesForProjectRouteAndTagName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
