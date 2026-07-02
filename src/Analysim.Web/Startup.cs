93a3b92995c429eaa151808c478eddbd11449b04
Mohab <mohab_sobhy@outlook.com>
2026-07-02 13:50:21 +0300

    Upgrade solution from .NET 6 to .NET 10

--- src/Analysim.Web/Startup.cs
+++ src/Analysim.Web/Startup.cs
@@ -1,4 +1,4 @@
-using AutoMapper;
+using System.Collections.Generic;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi;
 using Web.Extensions;
 
 namespace Web
@@ -31,34 +32,27 @@ namespace Web
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                c.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Title = "AnalySim API",
                     Version = "v1"
                 });
 
-                c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
                     Name = "Authorization",
-                    Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+                    Type = SecuritySchemeType.ApiKey,
                     Scheme = "Bearer",
                     BearerFormat = "JWT",
-                    In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                    In = ParameterLocation.Header,
                     Description = "Enter 'Bearer' followed by your JWT token (e.g. 'Bearer eyJhbGci...')"
                 });
 
-                c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+                c.AddSecurityRequirement(document => new OpenApiSecurityRequirement
                 {
                     {
-                        new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-                        {
-                            Reference = new Microsoft.OpenApi.Models.OpenApiReference
-                            {
-                                Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new string[] {}
+                        new OpenApiSecuritySchemeReference("Bearer", document, null),
+                        new List<string>()
                     }
                 });
             });
@@ -75,8 +69,6 @@ namespace Web
             services.ConfigureLoggerService();
 
             services.ConfigureMailService(Configuration);
-
-            services.AddAutoMapper(typeof(Startup));
 
             services.AddControllers(config =>
             {
